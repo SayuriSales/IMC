a9aa77c755d33dbb864ac79f7e8454f8b244a824
logonrm <logonrm@fiap.com.br>
2018-03-02 22:32:01 -0300

    Criação da lógica para calcular imc

--- app/src/main/java/com/sales/sayuri/imc/ResultadoActivity.kt
+++ app/src/main/java/com/sales/sayuri/imc/ResultadoActivity.kt
@@ -2,6 +2,8 @@ package com.sales.sayuri.imc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import com.sales.sayuri.imc.extension.format
 import kotlinx.android.synthetic.main.activity_resultado.*
 
 class ResultadoActivity : AppCompatActivity() {
@@ -10,11 +12,43 @@ class ResultadoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
 
-        val peso = intent?.getStringExtra("PESO")?.toDouble()
-        val altura = intent?.getStringExtra("ALTURA")?.toDouble()
+        val peso = intent?.getStringExtra("PESO")!!.toDouble()
+        val altura = intent?.getStringExtra("ALTURA")!!.toDouble()
 
-        val imc =peso?.div(altura!!.times(altura))
+        //val imc =peso?.div(altura!!.times(altura))
+        val imc = peso / (altura*altura)
 
-        tvIMC.setText(imc.toString().substring(0,3))
+       // tvIMC.setText(imc.toString().substring(0,3))
+
+            tvIMC.text = imc?.format(2)
+
+
+        if (imc < 17){
+            alterarTextoStatusIMC("Muito abaixo do peso")
+            alterarImagemStatusIMC(R.drawable.magreza)
+        }else if(imc <= 18.5) {
+            alterarTextoStatusIMC("Abaixo do peso")
+            alterarImagemStatusIMC(R.drawable.abaixo)
+        }else if(imc <= 24.9) {
+            alterarTextoStatusIMC("Peso ideal")
+            alterarImagemStatusIMC(R.drawable.ideal)
+        }else if(imc <= 29.9) {
+            alterarTextoStatusIMC("Acima do peso")
+            alterarImagemStatusIMC(R.drawable.sobre)
+        }else if(imc <= 34.9) {
+            alterarTextoStatusIMC("Obesidade")
+            alterarImagemStatusIMC(R.drawable.obesidade)
+        } else {
+            alterarTextoStatusIMC("Obesidade")
+            alterarImagemStatusIMC(R.drawable.obesidade)
+        }
     }
+
+    fun alterarImagemStatusIMC(idImagem: Int) {
+        ivStatusIMC.setImageDrawable(ContextCompat.getDrawable(this, idImagem))
+    }
+    fun alterarTextoStatusIMC(texto:String) {
+        tvStatusIMC.text = texto
+    }
+
 }
